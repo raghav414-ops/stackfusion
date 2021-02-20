0999b15a0ec8623930b406b3b7046a5b6eaa819c
raghav414-ops <79161456+raghav414-ops@users.noreply.github.com>
2021-02-20 08:16:28 +0530

    Create IotCode.c

---
+++ IotCode.c
@@ -0,0 +1,18 @@
+/*
+ * stackfusion.c
+ *
+ * Created: 20-02-2021 01:26:39
+ *  Author: Raghav
+ */ 
+
+
+#include <avr/io.h>
+#include "jsmn.h"
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
